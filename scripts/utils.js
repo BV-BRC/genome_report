9285a905d68ae5a8478a399acbaf9b17a85b5384
nconrad <nealconrad@gmail.com>
2018-03-02 17:12:45 -0600

    start utils module

---
+++ scripts/utils.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+/**
+ *
+ * utils.js
+ *
+ * Utitilty functions for genome-report file organization and such.
+ *
+ * Author(s):
+ *      nconrad
+*/
+
+const fs = require('fs'),
+    path = require('path'),
+    process = require('process'),
+    Promise = require("bluebird");
+
+const writeFile = Promise.promisify(fs.writeFile);
+
+const config = require('../config.json');
+
+
+function createGenomeFolder(id) {
+    let baseDir = path.resolve(`../${config.reportDir}`);
+
+    // create reports directory if needed
+    if (!fs.existsSync(baseDir)){
+        console.log(`\ncreating genome directory ${baseDir} ...` )
+        fs.mkdirSync(baseDir);
+    }
+
+    // create genome directory if needed
+    let genomeDir = path.resolve(`${baseDir}/${id}/`);
+    if (!fs.existsSync(genomeDir)){
+        console.log(`creating genome dir ${genomeDir} ...` )
+        fs.mkdirSync(genomeDir);
+    }
+
+    return genomeDir;
+}
+
+
+module.exports = {
+    createGenomeFolder: createGenomeFolder,
+    writeFile: writeFile
+}
