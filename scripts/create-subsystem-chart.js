07cc221852a65ec86a7d32c4476f6dd380eb7daf
nconrad <nealconrad@gmail.com>
2018-04-02 16:17:21 -0500

    add subsystem piechart

--- scripts/create-subsystem-chart.js
+++ scripts/create-subsystem-chart.js
@@ -20,100 +20,68 @@ const fs = require('fs'),
 
 const config = require('../config.json');
 const utils = require('./utils');
-const pieChart = require('../lib/pie-chart')
+const pieChart = require('../lib/pie-chart');
 const reqOpts = utils.requestOpts;
 
 
 if (require.main === module){
-    opts.option('-g, --genome_id [value]', 'Genome ID to fetch data for.')
-        .option('-t, --token [value]', 'Auth token (for private data)')
+    opts.option('-i, --input [value] Path to Genome Typed Object that contains subsystem data')
+        .option('-o, --output [value] Path to write resulting subsystem chart')
         .parse(process.argv)
 
-    if (!opts.genome_id) {
-        console.error("\nMust provide a genome ID.\n");
-        process.exit(1);
+    if (!opts.input) {
+        console.error("\nMust provide path '-i' to data (genome typed object)\n");
+        opts.outputHelp();
+        return 1;
     }
 
-    let genomeID = opts.genome_id;
-    createChart(genomeID, opts.token);
+    if (!opts.output) {
+        console.error("\nMust provide output path '-o' for subsystem SVG\n");
+        opts.outputHelp();
+        return 1;
+    }
+
+    fsCreateChart(opts.input, opts.output);
 }
 
 
-async function createChart(genomeID, token) {
-    reqOpts.headers.authorization = token;
+async function fsCreateChart(input, output) {
 
-    console.log('fetching subsystem data')
-    let data = await getSubsystemData(genomeID);
+    console.log('Reading genome object for subsystem data...');
+    let d = await utils.readFile(input, 'utf8');
+    let data = JSON.parse(d);
 
-    console.log('creating pie chart...');
-    let svg = pieChart(data);
-
-    let genomeDir = utils.createGenomeDir(genomeID);
+    console.log('Creating pie chart...')
+    let svg = await createChart(data);
 
     console.log('writing pie chart to file....');
-    await utils.writeFile(`${genomeDir}/${genomeID}-subsystem.svg`, svg, 'utf8');
+    await utils.writeFile(output, svg, 'utf8');
 }
 
 
-function getSubsystemData(genomeID) {
+async function createChart(genomeTypedObject) {
+    let subsystemSummary = genomeTypedObject.genome_quality_measure.subsystem_summary;
+    let subsystemData = parseSubsystemSummary(subsystemSummary)
 
-    var json = {
-        "stat": {
-            "type": "field",
-            "field": "superclass",
-            "limit": -1,
-            "facet": {
-                "subsystem_count": "unique(subsystem_id)",
-                "class": {
-                    "type": "field",
-                    "field": "class",
-                    "limit": -1,
-                    "facet": {
-                        "subsystem_count": "unique(subsystem_id)",
-                        "gene_count": "unique(feature_id)",
-                        "subclass": {
-                            "type": "field",
-                            "field": "subclass",
-                            "limit": -1,
-                            "facet": {
-                                "subsystem_count": "unique(subsystem_id)",
-                                "gene_count": "unique(feature_id)"
-                            }
-                        }
-                    }
-                }
-            }
+    let svg = pieChart({data: subsystemData});
+
+    return svg;
+}
+
+
+function parseSubsystemSummary(obj) {
+    let data = Object.keys(obj).map(key => {
+        let o = obj[key];
+
+        return {
+            name: key + ` (${o.subsystems}, ${o.genes})`,
+            value: o.subsystems
         }
-    }
-
-    // to be replaced
-    let url = `${config.dataAPIUrl}/subsystem/` +
-        `?eq(genome_id,520456.3)&group((field,subsystem_id),(format,simple),(ngroups,true),(limit,1),(facet,true))`+
-        `&json(facet,%7B%22stat%22%3A%7B%22type%22%3A%22field%22%2C%22field%22%3A%22superclass`+
-        `%22%2C%22limit%22%3A-1%2C%22facet%22%3A%7B%22subsystem_count%22%3A%22unique(subsystem_id)`+
-        `%22%2C%22class%22%3A%7B%22type%22%3A%22field%22%2C%22field%22%3A%22class%22%2C%22limit`+
-        `%22%3A-1%2C%22facet%22%3A%7B%22subsystem_count%22%3A%22unique(subsystem_id)%22%2C%22gene_count`+
-        `%22%3A%22unique(feature_id)%22%2C%22subclass%22%3A%7B%22type%22%3A%22field%22%2C%22field%22%3A%22`+
-        `subclass%22%2C%22limit%22%3A-1%2C%22facet%22%3A%7B%22subsystem_count%22%3A%22unique(subsystem_id)`+
-        `%22%2C%22gene_count%22%3A%22unique(feature_id)%22%7D%7D%7D%7D%7D%7D%7D)`+
-        `&http_accept=application/solr+json`;
-
-
-    console.log(`fetching subsystem data...`)
-    return rp.get(url, reqOpts).then(res => {
-        let buckets = res.facets.stat.buckets;
-
-        let data = buckets.map(b => {
-            return {
-                name: b.val,
-                value: b.count
-            }
-        })
-
-        return data;
-    }).catch((e) => {
-        console.error(e.message);
     })
+
+    data.sort((a, b) => b.value - a.value );
+
+    return data;
 }
 
 
