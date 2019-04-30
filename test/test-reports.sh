ed76749e2bd17645d6fea11290443e3eb8aab5a3
nconrad <nconrad@anl.gov>
2019-04-30 16:59:56 -0500

    update test GTOs and corresponding output

--- test/test-reports.sh
+++ test/test-reports.sh
@@ -1,4 +1,14 @@
 
+# test-reports.sh
+#
+# This is simple script to create reports for a few different objects
+#
+# Note:
+#   To get the latest GTOs for "test-report-amr.html",
+#   I've been running https://patricbrc.org/app/ComprehensiveGenomeAnalysis
+#   with "Staphylococcus aureus strain VB4283.contigs.fasta"
+
+
 ./scripts/create-report.js -i sample-data/sample.genome -o reports/test-report.html -c sample-data/myco.svg -s sample-data/myco.ss-colors -t sample-data/CodonTree.svg
 printf "\n"
 
